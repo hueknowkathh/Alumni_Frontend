6825bbe09f7d99b0aad771e7c40bcb12f1544c6c
hueknowkathh <kathlein.quimay@jmc.edu.ph>
2026-05-08 12:56:53 +0800

    google login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'register_screen.dart';
